3ef81e721dea1a23e95bc3cd9e59e4ece8b5128e
Yiming Liu <liuy379@uw.edu>
2019-04-30 00:08:33 -0700

    4.30

--- dataframe.py
+++ dataframe.py
@@ -1,19 +1,19 @@
 """
 (a) create a python module dataframe.py that reads the data in homework 2
-(b) dataframpe.py should generate an ValueError execption if the dataframe doesn't have the expected column names
+(b) dataframpe.py should generate an ValueError execption if the dataframe doesn't
+    have the expected column names
 'http://samplecsvs.s3.amazonaws.com/Sacramentorealestatetransactions.csv'
 """
 
 import pandas as pd
 
 def read_data(url):
-    dt = pd.read_csv(url)
+    """ this function reads data into a dataframe """
+    target = pd.read_csv(url)
     exp_names = ['street', 'city', 'zip', 'state', 'beds', 'baths', 'sq__ft', 'type',
-             'sale_date', 'price', 'latitude', 'longitude']
-    
-    if list(dt) == exp_names:
+                 'sale_date', 'price', 'latitude', 'longitude']
+    if list(target) == exp_names:
         pass
     else:
         raise ValueError("Dataframe doesn't have the expected column names")
-    
-    return dt
+    return target
--- test_dataframe.py
+++ test_dataframe.py
@@ -8,31 +8,29 @@ Tests being performed:
 - The dataframe has at least one row.
 """
 
-import pandas
 import unittest
 from dataframe import read_data
 
 class UnitTests(unittest.TestCase):
-
-    # The dataframe has the expected columns
+    """ Testcases for dataframe.py """
     def test_success(self):
-        dt = read_data('http://samplecsvs.s3.amazonaws.com/Sacramentorealestatetransactions.csv')
+        """ The dataframe has the expected columns """
+        target = read_data('http://samplecsvs.s3.amazonaws.com/'
+                           'Sacramentorealestatetransactions.csv')
         exp_names = ['street', 'city', 'zip', 'state', 'beds', 'baths', 'sq__ft', 'type',
                      'sale_date', 'price', 'latitude', 'longitude']
-        self.assertTrue(list(dt) == exp_names)
-    
-    # There are no nan values
+        self.assertTrue(list(target) == exp_names)
     def test_success1(self):
-        dt = read_data('http://samplecsvs.s3.amazonaws.com/Sacramentorealestatetransactions.csv')
-        null_sum = dt.isnull().sum().sum()
+        """ There are no nan values """
+        target = read_data('http://samplecsvs.s3.amazonaws.com/'
+                           'Sacramentorealestatetransactions.csv')
+        null_sum = target.isnull().sum().sum()
         self.assertEqual(null_sum, 0)
-        
-    # The dataframe has at least one row    
     def test_success2(self):
-        dt = read_data('http://samplecsvs.s3.amazonaws.com/Sacramentorealestatetransactions.csv')
-        self.assertTrue(len(dt) > 1)
-        
-    
+        """ The dataframe has at least one row """
+        target = read_data('http://samplecsvs.s3.amazonaws.com/'
+                           'Sacramentorealestatetransactions.csv')
+        self.assertTrue(len(target) > 1)
 
-# if __name__ == '__main__':
-#     unittest.main()
+if __name__ == '__main__':
+    unittest.main()
